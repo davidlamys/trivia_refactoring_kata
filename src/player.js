02acaec0dff9a77cbb62a8341ee72ebcd259e51f
David Lam <davidlys@gmail.com>
2018-09-19 00:22:49 +0800

    Add purse and method to increment purse

--- src/player.js
+++ src/player.js
@@ -1,11 +1,16 @@
 module.exports = function Player(name) {
   this.name = name;
   this.place = 0;
+  this.purse = 0;
 
   this.movePlayer = function(roll) {
     this.place += roll;
     if (this.place > 11) {
       this.place -= 12;
     }
+  }
+
+  this.incrementPurse = function() {
+    this.purse += 1;
   }
 }
