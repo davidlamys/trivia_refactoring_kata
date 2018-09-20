5214d74dad820f8c00b14312cc795c42db25caa1
David Lam <davidlys@gmail.com>
2018-09-20 20:54:48 +0800

    Replace string for questions categories with const

--- src/board.js
+++ src/board.js
@@ -4,36 +4,37 @@ module.exports = function Board() {
   const sportsQuestions = [];
   const rockQuestions = [];
 
+  const pop = "Pop";
+  const science = "Science";
+  const sports = "Sports";
+  const rock = "Rock";
+
   const generateQuestion = function(category, index) {
     return category + " Question " + index
   }
 
   for (var i = 0; i < 50; i++) {
-    popQuestions.push(generateQuestion("Pop", i));
-    scienceQuestions.push(generateQuestion("Science", i));
-    sportsQuestions.push(generateQuestion("Sports", i));
-    rockQuestions.push(generateQuestion("Rock", i));
+    popQuestions.push(generateQuestion(pop, i));
+    scienceQuestions.push(generateQuestion(science, i));
+    sportsQuestions.push(generateQuestion(sports, i));
+    rockQuestions.push(generateQuestion(rock, i));
   }
 
   this.getCategory = function(currentPlace) {
     switch (currentPlace%4) {
-      case 0:
-        return "Pop";
-      case 1:
-        return "Science";
-      case 2:
-        return "Sports";
-      case 3:
-        return "Rock";
+      case 0: return pop;
+      case 1: return science;
+      case 2: return sports;
+      case 3: return rock;
     }
   };
 
   this.getQuestion = function(currentPlace) {
       const currentCategory = this.getCategory(currentPlace);
-      if (currentCategory == "Pop") return (popQuestions.shift());
-      if (currentCategory == "Science") return (scienceQuestions.shift());
-      if (currentCategory == "Sports") return (sportsQuestions.shift());
-      if (currentCategory == "Rock") return (rockQuestions.shift());
+      if (currentCategory == pop) return (popQuestions.shift());
+      if (currentCategory == science) return (scienceQuestions.shift());
+      if (currentCategory == sports) return (sportsQuestions.shift());
+      if (currentCategory == rock) return (rockQuestions.shift());
   }
 
 };
